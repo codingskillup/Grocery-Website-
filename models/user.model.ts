303d5ab8d12110f5a2201b0d838a5ec732086076
ZohaibArshad45 <Zohaibfta@gmail.com>
2026-07-05 17:22:49 +0500

    Create User Model

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,1 +1,42 @@
 import mongoose from "mongoose";
+
+interface IUser{
+    _id?: mongoose.Types.ObjectId
+    name:string,
+    email:string
+    password:string
+    mobile?:string
+    role:"user" | "deliverBoy" | "admin"
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    mobile:{
+        type:String,
+        // required:true
+    },
+    role:{
+        type:String,
+        enum :["user", "deliverBoy", "admin"],
+        default:"user"
+    }
+
+
+}, { timestamps: true });
+
+
+const userModel = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default userModel;
